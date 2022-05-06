86831a5ca79bca2d552c23d3f72a72453281622a
cindy <cynthia.ms.chiu@gmail.com>
2022-05-06 15:50:51 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,103 @@
+<div align="center" id="top"> 
+  <img src="./.github/app.gif" alt="3D Art Gallery" />
+
+  &#xa0;
+
+  <a href="https://cynthia-3d-art-gallery.netlify.app/">Demo</a>
+</div>
+
+<h1 align="center">Art Gallery</h1>
+
+<p align="center">
+  <img alt="Github top language" src="https://img.shields.io/github/languages/top/cynthiachiu/3D-Art-Gallery?color=56BEB8">
+
+  <img alt="Github language count" src="https://img.shields.io/github/languages/count/cynthiachiu/3D-Art-Gallery?color=56BEB8">
+
+  <img alt="Repository size" src="https://img.shields.io/github/repo-size/cynthiachiu/3D-Art-Gallery?color=56BEB8">
+
+  <img alt="License" src="https://img.shields.io/github/license/cynthiachiu/3D-Art-Gallery?color=56BEB8">
+
+  <!-- <img alt="Github issues" src="https://img.shields.io/github/issues/{{YOUR_GITHUB_USERNAME}}/art-gallery?color=56BEB8" /> -->
+
+  <!-- <img alt="Github forks" src="https://img.shields.io/github/forks/{{YOUR_GITHUB_USERNAME}}/art-gallery?color=56BEB8" /> -->
+
+  <!-- <img alt="Github stars" src="https://img.shields.io/github/stars/{{YOUR_GITHUB_USERNAME}}/art-gallery?color=56BEB8" /> -->
+</p>
+
+<!-- Status -->
+
+<!-- <h4 align="center"> 
+	🚧  Art Gallery 🚀 Under construction...  🚧
+</h4> 
+
+<hr> -->
+
+<p align="center">
+  <a href="#dart-about">About</a> &#xa0; | &#xa0; 
+  <a href="#sparkles-features">Features</a> &#xa0; | &#xa0;
+  <a href="#rocket-technologies">Technologies</a> &#xa0; | &#xa0;
+  <a href="#white_check_mark-requirements">Requirements</a> &#xa0; | &#xa0;
+  <a href="#checkered_flag-starting">Starting</a> &#xa0; | &#xa0;
+  <a href="#memo-license">License</a> &#xa0; | &#xa0;
+  <a href="https://github.com/{{YOUR_GITHUB_USERNAME}}" target="_blank">Author</a>
+</p>
+
+<br>
+
+## :dart: About ##
+
+An interactive, 3D art gallery featuring paintings and sketchings that I have done over the past while. This is a React-based application that leverages React-Three-Fiber to render my art in 3D with moving spotlights, smooth horizontal scrolling, interactive mouse movement, realistic lighting with shadows, and postprocessing to add effects to the scene including noise and filters.
+
+See and play with my deployed demo at: https://cynthia-3d-art-gallery.netlify.app/
+
+![Alt Text](demo.gif)
+
+## :sparkles: Features ##
+
+:heavy_check_mark: React-based application using 3D web technology;\
+:heavy_check_mark: 3D Rendering of my paintings and sketches;\
+:heavy_check_mark: Interactive mouse movement;\
+:heavy_check_mark: Moving spotlights that highlight the current piece of art and cast realistic shadows;\
+:heavy_check_mark: Postprocessing scene effects;
+
+## :rocket: Technologies ##
+
+The following tools were used in this project:
+
+- [React-Three-Fiber](https://github.com/pmndrs/react-three-fiber)
+- [React-Three-Drei](https://github.com/pmndrs/drei)
+- [Node.js](https://nodejs.org/en/)
+- [React](https://pt-br.reactjs.org/)
+
+## :white_check_mark: Requirements ##
+
+Before starting :checkered_flag:, you need to have [Git](https://git-scm.com) and [Node](https://nodejs.org/en/) installed.
+
+## :checkered_flag: Starting ##
+
+```bash
+# Clone this project
+$ git clone https://github.com/cynthiachiu/3D-Art-Gallery
+
+# Access
+$ cd 3D-Art-Gallery
+
+# Install dependencies
+$ npm i
+
+# Run the project
+$ npm start
+
+# The server will initialize in the <http://localhost:3000>
+```
+
+## :memo: License ##
+
+This project is under license from MIT. For more details, see the [LICENSE](LICENSE.md) file.
+
+
+Made with :heart: by <a href="https://github.com/cynthiachiu" target="_blank">cynthiachiu</a>
+
+&#xa0;
+
+<a href="#top">Back to top</a>
---
+++ src/App.js
@@ -0,0 +1,191 @@
+import { Canvas, useFrame, useLoader, useThree } from '@react-three/fiber'
+import React, { Suspense } from 'react'
+import { SpotLight, Text, ScrollControls, Scroll, Html } from '@react-three/drei'
+import { EffectComposer, Noise, Vignette } from '@react-three/postprocessing'
+import { TextureLoader, Vector3 } from 'three'
+
+const ART_PIECES = [
+  {
+    title: 'Beautiful Beast',
+    imgPath: '/beauty_and_beast.jpeg',
+  },
+  {
+    title: 'Stillness',
+    imgPath: '/crane.jpeg',
+  },
+  {
+    title: 'Foxy',
+    imgPath: '/foxy.jpeg',
+  },
+  {
+    title: 'Gentle Giant',
+    imgPath: '/horse_sketch.jpeg',
+  },
+  {
+    title: 'Purity',
+    imgPath: '/kindness.jpeg',
+  },
+  {
+    title: 'Lonely Together',
+    imgPath: '/lonely_together.jpeg',
+  },
+  {
+    title: 'Owl',
+    imgPath: '/owl.jpeg',
+  },
+  {
+    title: 'Menace',
+    imgPath: '/panther.jpeg',
+  },
+  {
+    title: 'Paradise',
+    imgPath: '/paradise.jpeg',
+  },
+  {
+    title: 'Friendship',
+    imgPath: '/sprited_away.jpeg',
+  },
+  {
+    title: 'Wanderlust',
+    imgPath: '/wonder.jpeg',
+  },
+]
+
+const WallArt = (props) => {
+  const { art, i } = props
+  const { width: w, height: h } = useThree((state) => state.viewport);
+  const gap = 4;
+  const imageWidth = 3;
+  const texture = useLoader(TextureLoader, art.imgPath)
+
+  return (
+    <>
+      <group>
+        <SpotLight
+          position={[(i + 1) * (imageWidth + gap) + (i + 1) - w / 4, 2.5, 1]}
+          penumbra={1}
+          angle={0.6}
+          attenuation={1}
+          anglePower={5}
+          intensity={20}
+          distance={10}
+          castShadow
+          color={0xffffff}
+        />
+        <mesh castShadow position={[(i + 1) * (imageWidth + gap) + (i + 1), 0, 0]}>
+          <boxBufferGeometry attach="geometry" args={[imageWidth, h / 2, 0.07]} />
+          <meshStandardMaterial
+            attach="material"
+            map={texture}
+            roughness={0.2}
+            metalness={0.8}
+          />
+        </mesh>
+
+        <mesh position={[(i + 1) * (imageWidth + gap) + (i + 1), -2.5, 0]}>
+          <planeGeometry args={[1.25, 0.5]} />
+          <meshStandardMaterial color={0xFAEBD7} />
+          <Text
+            position-z={0}
+            scale={[2, 2, 2]}
+            color="black"
+            anchorX="center"
+            anchorY="middle"
+            font="https://fonts.gstatic.com/s/sacramento/v5/buEzpo6gcdjy0EiZMBUG4C0f-w.woff"
+          >
+            {art.title}
+          </Text>
+        </mesh>
+      </group>
+    </>
+  )
+}
+
+const Scene = () => {
+  return (
+    <Suspense fallback={
+      <Html style={{ fontSize: '6vw', whiteSpace: 'nowrap', color: 'white' }} center>
+        Loading 3D Art Gallery...
+      </Html>
+    }>
+      <ScrollControls infinite horizontal damping={4} pages={7} distance={1}>
+        <Scroll>
+          <Text
+            position-z={0}
+            anchorX="center"
+            anchorY="bottom"
+            scale={[4, 4, 4]}
+            color="#94A6FF"
+            font="https://fonts.gstatic.com/s/sacramento/v5/buEzpo6gcdjy0EiZMBUG4C0f-w.woff"
+            castShadow
+          >
+            Creativity is allowing yourself to make mistakes.
+          </Text>
+          <Text
+            position-z={1}
+            anchorX="center"
+            anchorY="top"
+            scale={[4, 4, 4]}
+            color="#FBA90A"
+            // font="https://fonts.gstatic.com/s/cookie/v8/syky-y18lb0tSbf9kgqU.woff"
+            font="https://fonts.gstatic.com/s/sacramento/v5/buEzpo6gcdjy0EiZMBUG4C0f-w.woff"
+            castShadow
+          >
+            Art is knowing which ones to keep.
+          </Text>
+          <Text
+            position={[0, -0.5, 1.5]}
+            anchorX="center"
+            anchorY="top"
+            font="https://fonts.gstatic.com/s/sacramento/v5/buEzpo6gcdjy0EiZMBUG4C0f-w.woff"
+          // castShadow
+          >
+            ~ Scott Adams
+          </Text>
+
+          {ART_PIECES.map((art, i) => {
+            return <WallArt
+              key={i}
+              i={i}
+              art={art}
+            />
+          })
+          }
+        </Scroll>
+      </ScrollControls>
+    </Suspense >
+  )
+}
+
+const Rig = () => {
+  const { camera, mouse } = useThree()
+  const vec = new Vector3()
+  return useFrame(() => camera.position.lerp(vec.set(mouse.x * 0.5, mouse.y * 0.5, camera.position.z), 0.2))
+}
+
+function App() {
+  return (
+    <Canvas shadows camera >
+      <ambientLight intensity={0.6} color={0xffffff} />
+
+      {/* This is the wall that supports shadows */}
+      <mesh
+        position={[0, 0, -0.1]}
+        receiveShadow
+      >
+        <planeGeometry args={[20, 15]} />
+        <meshStandardMaterial color={0x000000} />
+      </mesh>
+      <Scene />
+
+      <EffectComposer>
+        <Noise opacity={0.05} />
+        <Vignette eskil={false} offset={0.1} darkness={0.5} />
+      </EffectComposer>
+
+      <Rig />
+    </Canvas>
+  )
+}
+
+export default App;
